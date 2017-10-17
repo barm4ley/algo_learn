2582aa9c0768029d9a95c30350674858ef04e282
Maxim Musich <maxim@unity3d.com>
2017-10-17 19:47:54 +0300

    PEP-8 fixes

--- insertion_sort.py
+++ insertion_sort.py
@@ -16,11 +16,14 @@ def sort(lst):
 
 SORTED = [1, 2, 3, 4, 5]
 
+
 def test_inversed():
     assert sort([5,4,3,2,1]) == SORTED
 
+
 def test_presorted():
     assert sort(SORTED) == SORTED
+
 
 def test_random():
     assert sort([1,4,5,3,2]) == SORTED
